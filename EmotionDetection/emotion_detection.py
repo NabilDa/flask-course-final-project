a7c0c2ccf697f71f21a4c0d67f65287b190a6feb
NabilDa <naabil.daoui@gmail.com>
2025-07-24 14:33:22 -0400

    Modify files

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,9 +6,20 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myObj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json=myObj, headers=headers)
-    formatted_response = json.loads(response.text)
-    emotion_dict = formatted_response["emotionPredictions"][0]["emotion"]
-    dominant_emotion = max(emotion_dict, key=emotion_dict.get)
-    emotion_dict['dominant_emotion'] = dominant_emotion
-    return emotion_dict
+    
+    if response.status_code != 400:
+        formatted_response = json.loads(response.text)
+        emotion_dict = formatted_response["emotionPredictions"][0]["emotion"]
+        dominant_emotion = max(emotion_dict, key=emotion_dict.get)
+        emotion_dict['dominant_emotion'] = dominant_emotion
+    else:
+        emotion_dict = {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+        }
 
+    return emotion_dict
--- server.py
+++ server.py
@@ -1,10 +1,18 @@
-from EmotionDetection.emotion_detection import emotion_detector
+"""
+This is a Flask app for analyzing the emotions expressed in a given text.
+"""
 from flask import Flask, request, render_template
+from EmotionDetection.emotion_detection import emotion_detector
 
+# Initiating the Flask app
 app = Flask(__name__)
 
+# Defining the '/emotionDetector' route for analyzing the emotions
 @app.route("/emotionDetector")
 def send_emotion():
+    """
+    This is the logic that gets run when accessing the '/emotionDetector' endpoint
+    """
     text_to_analyze = request.args["textToAnalyze"]
     emotion_dict = emotion_detector(text_to_analyze)
     anger_score = emotion_dict["anger"]
@@ -14,12 +22,19 @@ def send_emotion():
     sadness_score = emotion_dict["sadness"]
     dominant_emotion = emotion_dict["dominant_emotion"]
 
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!"
+
     return f"For the given statement, the system response is 'anger': {anger_score}, \
             'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score}, \
             and 'sadness': {sadness_score}. The dominant emotion is {dominant_emotion}."
 
+# Defining the rout route
 @app.route("/")
 def main_page():
+    """
+    This function renders the index.html file when accessing the '/' route
+    """
     return render_template("index.html")
 
 if __name__ == "__main__":
