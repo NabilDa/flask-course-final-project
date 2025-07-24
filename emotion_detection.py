996b53de904a812886e8de82796ec4f5c4e71c09
NabilDa <naabil.daoui@gmail.com>
2025-07-24 04:31:14 -0400

    Modify emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 def emotion_detector(text_to_analyze):
@@ -5,4 +6,9 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myObj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json=myObj, headers=headers)
-    return response.text
+    formatted_response = json.loads(response.text)
+    emotion_dict = formatted_response["emotionPredictions"][0]["emotion"]
+    dominant_emotion = max(emotion_dict, key=emotion_dict.get)
+    emotion_dict['dominant_emotion'] = dominant_emotion
+    return emotion_dict
+
